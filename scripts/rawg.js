b22e215f4530e651978c24dcbc58056f8bb24750
taiwohusayn5@gmail.com <taiwohusayn5@gmail.com>
2021-09-28 21:09:46 +0100

    search game enpoint

--- scripts/rawg.js
+++ scripts/rawg.js
@@ -9,3 +9,15 @@ const funGames = async () => {
   return data;
 
 }
+
+const funGames = async () => {
+
+  const base = 'https://api.rawg.io/api/games';
+  const query = `?key=${key}`;
+
+  const response = await fetch(base + query);
+  const data = await response.json();
+
+  return data;
+
+}
