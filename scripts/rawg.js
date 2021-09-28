2958d6a2f512ea0678b33e676d94397c04b92c2f
taiwohusayn5@gmail.com <taiwohusayn5@gmail.com>
2021-09-28 21:46:05 +0100

    RAWG image

--- scripts/rawg.js
+++ scripts/rawg.js
@@ -10,14 +10,16 @@ const funGames = async () => {
 
 }
 
-const funGames = async () => {
+const searchGames = async (name) => {
 
   const base = 'https://api.rawg.io/api/games';
-  const query = `?key=${key}`;
+  const query = `?key=${key}&q=${name}`;
 
   const response = await fetch(base + query);
   const data = await response.json();
 
-  return data;
+  console.log(data);
 
 }
+
+searchGames("Portal 2")
